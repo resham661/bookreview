c223d5a45767ab0e568a76bf45dffa2aef9236f4
resham661 <reshamgg12@gmail.com>
2023-01-18 17:26:21 +0530

    third commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_114706) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_102658) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
