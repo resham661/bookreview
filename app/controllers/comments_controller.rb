84f594d6bcc1b37a91026ca156dad80052bd087b
resham661 <reshamgg12@gmail.com>
2023-02-16 16:52:05 +0530

    Added Comment section

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  before_action :find_book
+
+  def new
+    @comment = current_user.comments.new
+  end
+
+  def create
+    #create a new comment for the book from the current_user
+    @comment = Comment.new(comment_params)
+    @comment.book_id = @book.id
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      redirect_to book_path(@book)
+    else
+      # flash[:notice] = @comment.errors.full_messages.to_sentence
+      render 'new'
+    end
+  end
+
+  private
+
+    def comment_params
+      params
+        .require(:comment)
+        .permit(:content)
+        # .merge(book_id: params[:book_id])
+    end
+
+    def find_book
+      @book = Book.find(params[:book_id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_102658) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_093624) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -27,6 +27,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_102658) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "book_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_102658) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "books"
+  add_foreign_key "comments", "users"
 end
